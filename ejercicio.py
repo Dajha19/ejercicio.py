4fc5f4ecf5d156c86fc824db3c3f490b3fa336b0
Dajha19 <1509415@senati.pe>
2024-08-28 12:21:31 -0500

    ejercicio

--- ejercicio.py
+++ ejercicio.py
@@ -1,12 +1,12 @@
 import pandas as pd
 
-estudiantes_df = pd.DataFrame({
+estudiantes = pd.DataFrame({
     'Nombre': ['Juan', 'Ana', 'Luis', 'Marta'],
     'Edad': [15, 14, 16, 15],
     'Nota': [8.5, 9.0, 7.5, 8.0]
 })
 
-df = pd.DataFrame(estudiantes_df)
+df = pd.DataFrame(estudiantes)
 
 
 print(df.head())
@@ -18,15 +18,14 @@ print(primera_fila)
 # Añade una nueva columna
 import pandas as pd
 
-estudiantes_df = {
+estudiantes = {
     'Nombre': ['Juan', 'Ana', 'Luis', 'Marta'],
     'Edad': [15, 14, 16, 15],
-    
     'Nota': [8.5, 9.0, 7.5, 8.0], 
     'Ciudad' :[ 'Madrid', 'Barcelona', 'Valencia', 'Sevilla']
 }
 
-df = pd.DataFrame(estudiantes_df)
+df = pd.DataFrame(estudiantes)
 
 
 print(df.head())
